62bef93035debaa625711ac6942e75ed76b31886
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-08-16 13:01:13 +0100

    Finishing Card Lits of series and movies

--- src/containers/browse.js
+++ src/containers/browse.js
@@ -5,6 +5,8 @@ import Loading from '../components/loading/loading.component';
 import Header from '../components/header/header.component';
 import logo from '../logo.svg';
 import * as ROUTES from '../constants/routes';
+import Card from '../components/card/card.component';
+import Player from '../components/player/player.component';
 
 
 
@@ -24,6 +26,10 @@ const BrowseContainer = ({slides}) => {
       setLoading(false);
     },3000);
   },[profile.displayName]);
+
+  useEffect(()=>{
+    setSlideRows(slides[category]);
+  },[slides,category]);
 
   return (
     profile.displayName ? (
@@ -69,8 +75,35 @@ const BrowseContainer = ({slides}) => {
           <Header.PlayButton>Play</Header.PlayButton>
         </Header.Feature>
       </Header>
+
+      <Card.Group>
+       {slideRows.map((slideItem) => (
+         <Card key={`${category}-${slideItem.title.toLowerCase()}`}>
+           <Card.Title>{slideItem.title}</Card.Title>
+           <Card.Entities>
+             {slideItem.data.map((item) => (
+               <Card.Item key={item.docId} item={item}>
+                 <Card.Image src={`/images/${category}/${item.genre}/${item.slug}/small.jpg`} />
+                 <Card.Meta>
+                   <Card.SubTitle>{item.title}</Card.SubTitle>
+                   <Card.Text>{item.description}</Card.Text>
+                 </Card.Meta>
+               </Card.Item>
+             ))}
+           </Card.Entities>
+           <Card.Feature category={category}>
+             <Player>
+               <Player.Button />
+               <Player.Video src="/videos/bunny.mp4" />
+             </Player>
+           </Card.Feature>
+         </Card>
+       ))}
+     </Card.Group>
       </>
     ) :
+
+
     (<SelectProfileContainer user={user} setProfile={setProfile}/>)
   )
 }
