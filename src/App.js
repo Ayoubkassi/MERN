51bb9402508839c7134339f6b8308a8e0d023c4d
Ayoubkassi <ayoub.kassi@uit.ac.ma>
2021-08-16 08:19:51 +0100

    Fixing auth

--- src/App.js
+++ src/App.js
@@ -5,16 +5,39 @@ import HomePage from './pages/home';
 import BrowsePage from './pages/browse';
 import SignIn from './pages/signin';
 import SignUp from './pages/signup';
+import { IsUserRedirect , ProtectedRoute } from './helpers/routes';
 
 
 
 function App() {
+  const user = {};
   return (
     <Router>
-      <Route exact  path={ROUTES.HOME} component={HomePage} />
-      <Route exact  path={ROUTES.BROWSE} component={BrowsePage} />
-      <Route exact  path={ROUTES.SIGN_IN} component={SignIn} />
-      <Route exact  path={ROUTES.SIGN_UP} component={SignUp} />
+      <Route>
+        <IsUserRedirect user={user}
+                        loggedInPath={ROUTES.BROWSE}
+                        path={ROUTES.SIGN_IN}
+                        exact
+        />
+      </Route>
+      <Route>
+        <IsUserRedirect user={user}
+                        loggedInPath={ROUTES.BROWSE}
+                        path={ROUTES.SIGN_UP}
+                        exact
+        />
+      </Route>
+      <ProtectedRoute user={user} path={ROUTES.BROWSE}exact>
+        <BrowsePage />
+      </ProtectedRoute>
+      <Route>
+      <IsUserRedirect user={user}
+                        loggedInPath={ROUTES.BROWSE}
+                        path={ROUTES.HOME}
+                        exact
+        />
+      </Route>
+
     </Router>
   );
 }
---
+++ src/helpers/routes.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Route , Redirect } from 'react-router-dom';
+
+
+export const IsUserRedirect = ({ user  , loggedInPath , children , ...otherProps}) => {
+  return (
+    <Route {...otherProps} render={()=>{
+      if(!user){
+        return children;
+      }
+      if(user){
+        return (
+          <Redirect to={{pathname : loggedInPath}} />
+        );
+      }
+
+      return null;
+    }} />
+  )
+}
+
+
+export const ProtectedRoute = ({ user , children , ...otherProps}) => {
+  return (
+      <Route {...otherProps} render={({location})=>{
+        if(user){
+          return children;
+        }
+        if(!user){
+          return (<Redirect to={{pathname : 'signin', state : {from : location} }} />);
+        }
+        return null;
+      }} />
+  )
+}
